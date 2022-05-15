17128e4ee15985598320f36d3cf7c2786d6d4677
realvjy <realvjy@gmail.com>
2022-05-15 12:17:01 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Feather Figma plugin
+
+Quick access to [3dicons](https://feathericons.com) icons in Figma.
+
+👉 https://www.figma.com/c/plugin/744047966581015514/Feather
+
+
+![c70a8d59-917d-484d-a3b5-4db806f34710-cover](https://user-images.githubusercontent.com/4608155/63215499-2d7cc300-c0dc-11e9-9bfa-31567ab6e700.png)
+
+## Local development
+
+1. Clone the repository
+
+   ```shell
+   git clone 
+   cd 3dicons-plugin
+   ```
+
+1. Install the dependencies
+
+   ```shell
+   yarn
+   ```
+
+1. Build the plugin
+
+   ```
+   yarn watch
+   ```
+
+1. Open the [Figma desktop app](https://www.figma.com/downloads/)
+
+1. Go to `Menu > Plugins > Development > New Plugin...`
+
+1. Choose `3dicons-plugin/manifest.json`
+
+1. Run the plugin by going to `Menu > Plugins > Development > Feather`
---
+++ src/ui.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "./ui.css";
+
+declare function require(path: string): any;
+
+function App() {
+  const inputRef = React.useRef<HTMLInputElement>(null);
+
+  const onCreate = () => {
+    const count = Number(inputRef.current?.value || 0);
+    parent.postMessage(
+      { pluginMessage: { type: "create-rectangles", count } },
+      "*"
+    );
+  };
+
+  const onCancel = () => {
+    parent.postMessage({ pluginMessage: { type: "cancel" } }, "*");
+  };
+
+  return (
+    <main>
+      <header>
+        <img src={require("./logo.svg")} />
+        <h2>Rectangle Creator</h2>
+      </header>
+      <section>
+        <input id="input" type="number" min="0" ref={inputRef} />
+        <label htmlFor="input">Rectangle Count</label>
+      </section>
+      <footer>
+        <button className="brand" onClick={onCreate}>
+          Create
+        </button>
+        <button onClick={onCancel}>Cancel</button>
+      </footer>
+    </main>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("react-page"));
